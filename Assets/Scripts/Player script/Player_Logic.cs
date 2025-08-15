2ce430397920302643d4c6c75e1030636031ef26
AryanNag29 <231030332@juitsolan.in>
2025-08-16 04:13:01 +0530

    Character movement is done but need some tune

--- Assets/Scripts/Player script/Player_Logic.cs
+++ Assets/Scripts/Player script/Player_Logic.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class Player_Logic : MonoBehaviour
@@ -12,24 +13,31 @@ public class Player_Logic : MonoBehaviour
     Vector2 currentMovementInput;
     Vector3 currentMovement;
     bool isMovementPressed;
+    float movementSpeed = 5;
+
+    //functions
+    void onMovementInput(InputAction.CallbackContext Context)
+    {
+        //movement of character
+        currentMovementInput = Context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.y = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
 
     void Awake()
     {
         controls = GetComponent<CharacterController>();
         playerinput = new PlayerInput();
-        playerinput.CharacterControls.Move.started += Context =>
-        {   
-            //movement of character
-            currentMovementInput = Context.ReadValue<Vector2>();
-            currentMovement.x = currentMovementInput.x;
-            currentMovement.y = currentMovementInput.y;
-        };
+        playerinput.CharacterControls.Move.started += onMovementInput;
+        playerinput.CharacterControls.Move.canceled += onMovementInput;
+        playerinput.CharacterControls.Move.performed += onMovementInput;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        controls.Move(currentMovement *movementSpeed * Time.deltaTime);
     }
     void OnEnable()
     {
