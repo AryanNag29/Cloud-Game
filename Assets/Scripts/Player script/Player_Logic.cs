29d73568a9b7b3b80e23ca0a1efcbe2f7dc9bd30
AryanNag29 <231030332@juitsolan.in>
2025-08-18 01:10:10 +0530

    Fixed the gravity

--- Assets/Scripts/Player script/Player_Logic.cs
+++ Assets/Scripts/Player script/Player_Logic.cs
@@ -15,18 +15,31 @@ public class Player_Logic : MonoBehaviour
     private Vector3 _currentMovement;
     private bool _isMovementPressed;
     private float _movementSpeed = 5;
-    private float _gravity = 9.81f;
-    private float _gravityScale = 1;
-    private
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityScale = 1;
+    private float _velocity;
+
 
     //functions
-    void onMovementInput(InputAction.CallbackContext Context)
+    void onMovementInput(InputAction.CallbackContext Context) //for movement function
     {
         //movement of character
         _currentMovementInput = Context.ReadValue<Vector2>();
         _currentMovement.x = _currentMovementInput.x;
         _currentMovement.y = _currentMovementInput.y;
         _isMovementPressed = _currentMovementInput.x != 0 || _currentMovementInput.y != 0;
+    }
+    void applyGravity() // for gravity function
+    {
+        if (controls.isGrounded && _velocity<0)
+        {
+            _velocity = -1f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityScale * Time.deltaTime;
+        }
+        _currentMovement.y += _velocity;
     }
     
 
@@ -45,16 +58,7 @@ public class Player_Logic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (controls.isGrounded == true)
-        {
-            _gravity = 0;
-        }
-        else
-        {
-            _gravity += Physics.gravity.y * _gravityScale * Time.deltaTime;
-        }
-        transform.Translate(new Vector3(0, _gravity, 0)*Time.deltaTime);
+        applyGravity();
         controls.Move(_currentMovement * _movementSpeed * Time.deltaTime);
     }
     void OnEnable()
